4c187a413259aec6173b6aeeb84a79ecb72b3157
Mohamed <114217025+melhelow@users.noreply.github.com>
2023-09-10 18:59:12 -0500

    type in seeders

---
+++ seeders/receiptSeed.js
@@ -0,0 +1,24 @@
+[
+  {
+    "retailer": "SuperMart",
+    "purchaseDate": "2023-09-10",
+    "purchaseTime": "15:30",
+    "items": [
+      {
+        "shortDescription": "Product A",
+        "price": 10.99
+      },
+      {
+        "shortDescription": "Product B",
+        "price": 5.49
+      },
+      {
+        "shortDescription": "Product C",
+        "price": 3.99
+      }
+    ],
+    "total": 20.47
+  }
+  
+]
+  
